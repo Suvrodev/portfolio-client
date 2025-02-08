3ba71d3824145047757c3340bf19c25969314bd1
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-02-09 01:27:31 +0600

    Added About me

--- src/app/(with-user-dashboard)/page.tsx
+++ src/app/(with-user-dashboard)/page.tsx
@@ -1,3 +1,5 @@
+import "./page.css";
+import AboutMe from "@/components/AboutMe/AboutMe";
 import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
@@ -5,6 +7,10 @@ export default function Home() {
     <div className="w-full overflow-auto" id="banner">
       <div>
         <Banner />
+      </div>
+
+      <div className=" px-5 md:px-36 py-10 aboutmeBgColor" id="about">
+        <AboutMe />
       </div>
     </div>
   );
