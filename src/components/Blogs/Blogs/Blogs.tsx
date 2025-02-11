e7f7bf583eab745881520a56db5a5a5e392308cf
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-02-11 22:22:28 +0600

    make force cash for project in home page

--- src/components/Blogs/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs/Blogs.tsx
@@ -1,22 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import BlogBox from "../BlogBox/BlogBox";
-import { TBlog } from "@/utils/types/globalTypes";
+// import React, { useEffect, useState } from "react";
+// import { TBlog } from "@/utils/types/globalTypes";
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    fetch("/blogs.json")
-      .then((res) => res.json())
-      .then((data) => setBlogs(data));
-  }, []);
+  // const [blogs, setBlogs] = useState([]);
+  // useEffect(() => {
+  //   fetch("/blogs.json")
+  //     .then((res) => res.json())
+  //     .then((data) => setBlogs(data));
+  // }, []);
   return (
     <div className="overflow-hidden">
       <h1 className="text-2xl font-bold pText mb-10">Blog</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {blogs.map((blog: TBlog, idx: number) => (
+        {/* {blogs.map((blog: TBlog, idx: number) => (
           <BlogBox key={idx} blog={blog} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
--- src/components/Project/Project/Project.tsx
+++ src/components/Project/Project/Project.tsx
@@ -1,20 +1,19 @@
-"use client";
+import ProjectBox from "@/components/Admin/Project/ProjectBox/ProjectBox";
+import { TProject } from "@/utils/types/globalTypes";
 
-import { useEffect, useState } from "react";
-import ProjectBox from "../ProjectBox/ProjectBox";
-
-const Project = () => {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    fetch("/projects.json")
-      .then((res) => res.json())
-      .then((data) => setProjects(data));
-  }, []);
+const Project = async () => {
+  const res = await fetch("http://localhost:5000/api/project/", {
+    cache: "force-cache",
+  });
+  const data = await res.json();
+  // console.log("Data: ", data);
+  const projects = data?.data;
+  // console.log("Projects: ", projects);
   return (
     <div>
       <h1 className="text-2xl font-bold pText mb-10">Project</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {projects.map((project, idx) => (
+        {projects.map((project: TProject, idx: number) => (
           <ProjectBox key={idx} project={project} />
         ))}
       </div>
