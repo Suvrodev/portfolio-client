7f6bce8e5a22b4e7ea98525a49f3cb0d21f21b81
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-02-11 23:32:23 +0600

    Added blog page

--- src/app/(with-user-dashboard)/blog/page.tsx
+++ src/app/(with-user-dashboard)/blog/page.tsx
@@ -1,7 +1,22 @@
-const BlogPage = () => {
+import BlogCard from "@/components/Admin/Blog/BlogCard/BlogCard";
+import { TBlog } from "@/utils/types/globalTypes";
+
+const BlogPage = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/blog/`, {
+    next: { revalidate: 30 },
+  });
+  const data = await res.json();
+  // console.log("Data: ", data);
+  const blogs = data?.data;
+  //   console.log("Blog: ", blogs);
   return (
-    <div>
-      <h1>Blog Page</h1>
+    <div className="p-2 md:p-5  sColor">
+      <h1 className="text-2xl font-bold pText mb-10 text-white">Blogs</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {blogs.map((blog: TBlog, idx: number) => (
+          <BlogCard key={idx} blog={blog} />
+        ))}
+      </div>
     </div>
   );
 };
