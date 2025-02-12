54f5bef60678a894fa2464034d4caa18a28554bb
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-02-13 00:57:08 +0600

    All admin route make private in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,13 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard"] };
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/dashboard/message",
+    "/dashboard/add-project",
+    "/dashboard/all-project",
+    "/dashboard/add-blog",
+    "/dashboard/all-blog",
+    "/dashboard/resume",
+  ],
+};
